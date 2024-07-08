0b2d2a44d212d23443489ab4af5f43ce341ea326
GabOnezioProfissional <gabonezioprofissional@gmail.com>
2024-07-08 01:51:44 -0300

    The new configurations to sw and configurations to rollup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      DATABASE_URL: 'postgres://GabOnezio:GhostBoy21!!!@localhost:5432/sorafm',
+    },
+    // Outras configurações do Next.js podem ser adicionadas aqui
+  };
+  
+  export default nextConfig;
